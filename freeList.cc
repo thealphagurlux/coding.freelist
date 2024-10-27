7b5606709644cb6c0e26b392f7478bed0812aea2
drdoubleu <143204165+thealphagurlux@users.noreply.github.com>
2024-10-27 22:55:03 +0000

    done

--- freeList.cc
+++ freeList.cc
@@ -1,24 +1,61 @@
 #include "freeList.h"
 
 freeList::freeList( long int*inRAM, int size ) {
+  long int * head = inRAM;
+
+  int Msize = size;
+
 }
 
 long int*
 freeList::reserve_space( int size ) {
-  long int* loc = NULL;
+  
+  long int* reservedMemory = (long int*)malloc((size + 2) * sizeof(long int));
+  if (!reservedMemory) {
+    std::cerr << "Memory allocation failed!" << std::endl;
+    return nullptr; 
+   }
+  head = reservedMemory + 2;
 
-  return loc;
+  reservedMemory[0] = size;
+  reservedMemory[1] = 0;
+
+  return head; 
 }
 
 void
 freeList::free_space( long int* location ) {
+  long int* header = location - 2;
+  header[1] = (long int)head; 
+  head = header; 
 }
 
 void
 freeList::coalesce_forward() {
+long int* current = head;
+
+while (current) {
+            
+  int size = current[0];
+
+  
+  long int* nextChunk = (long int*)((char*)current + (size + 2) * sizeof(long int));
+
+  
+  if (nextChunk[0] > 0) {
+                
+    current[0] += nextChunk[0] + 2; 
+    current[1] = nextChunk[1]; 
+  } else {
+    
+    current = nextChunk;
+   }
+  }
+
+
 }
 
-// print() is given to you. It traverses the free list and prints out what is there.
+
 void freeList::print() {
   long int * printHead = head;
   
